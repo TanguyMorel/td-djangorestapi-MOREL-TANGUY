ec4c35d779ab705c025d0a8714ea5e6c0f3d33f3
Tanguy <tanguymorel.pro@gmail.com>
2024-06-26 09:53:44 +0200

    add: 3 views in projects

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Researcher, ResearchProject, Publication
+from .serializers import ResearcherSerializer, ResearchProjectSerializer, PublicationSerializer
 
-# Create your views here.
+class ResearcherViewSet(viewsets.ModelViewSet):
+    queryset = Researcher.objects.all()
+    serializer_class = ResearcherSerializer
+
+class ResearchProjectViewSet(viewsets.ModelViewSet):
+    queryset = ResearchProject.objects.all()
+    serializer_class = ResearchProjectSerializer
+
+class PublicationViewSet(viewsets.ModelViewSet):
+    queryset = Publication.objects.all()
+    serializer_class = PublicationSerializer
