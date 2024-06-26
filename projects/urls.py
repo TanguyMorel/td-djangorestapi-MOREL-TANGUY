6952d05e41bcdcb8f93afb88098426a35775994c
Tanguy <tanguymorel.pro@gmail.com>
2024-06-26 11:56:54 +0200

    add: mise en place du front

--- projects/urls.py
+++ projects/urls.py
@@ -1,14 +1,19 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import ResearcherViewSet, ResearchProjectViewSet, PublicationViewSet, home
+from . import views
 
 router = DefaultRouter()
-router.register(r'researchers', ResearcherViewSet)
-router.register(r'projects', ResearchProjectViewSet)
-router.register(r'publications', PublicationViewSet)
+router.register(r'researchers', views.ResearcherViewSet)
+router.register(r'projects', views.ResearchProjectViewSet)
+router.register(r'publications', views.PublicationViewSet)
 
 urlpatterns = [
-    path('', home, name='home'), 
-    path('', include(router.urls)),
+    # path('home', views.home, name='home'),
+    path('', views.home, name='home'),
+    # path('', include(router.urls)), <-----------
+    path('projects/', views.project_list, name='projects_list.html'),
+    # path('researchers/', views.researcher_list, name='researcher_list'),
+    # path('publications/', views.publication_list, name='publication_list'),
 ]
 
--- projects/views.py
+++ projects/views.py
@@ -4,10 +4,8 @@ from .models import Researcher, ResearchProject, Publication
 from .serializers import ResearcherSerializer, ResearchProjectSerializer, PublicationSerializer
 from django.http import HttpResponse
 
-def home(request):
-    return HttpResponse("Bienvenue sur la page d'accueil des projets de recherche!")
 
-
+# ViewSets pour l'API REST
 class ResearcherViewSet(viewsets.ModelViewSet):
     queryset = Researcher.objects.all()
     serializer_class = ResearcherSerializer
@@ -19,3 +17,29 @@ class ResearchProjectViewSet(viewsets.ModelViewSet):
 class PublicationViewSet(viewsets.ModelViewSet):
     queryset = Publication.objects.all()
     serializer_class = PublicationSerializer
+
+# Views pour le front
+
+
+def home(request):
+    return render(request, 'projects/home.html')
+
+# def home(request):
+
+#         # home = ResearchProject.objects.all()
+#         # return render(request, 'home.html', {'projects': home})
+#     return HttpResponse("Bienvenue sur la page d'accueil des projets de recherche!")
+
+def project_list(request):
+    projects = ResearchProject.objects.all()
+    return render(request, 'projects/projects_list.html', {'projects': projects})
+    # return HttpResponse("Yopdilp")
+
+
+def researcher_list(request):
+    researchers = Researcher.objects.all()
+    return render(request, 'projects/researcher_list.html', {'researchers': researchers})
+
+def publication_list(request):
+    publications = Publication.objects.all()
+    return render(request, 'projects/publication_list.html', {'publications': publications})
