b3f59e818be33ef84882fd6330c9a9c7d17b35ff
Tanguy <tanguymorel.pro@gmail.com>
2024-06-26 09:44:55 +0200

    add: 3 models dans projects

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class Researcher(models.Model):
+    name = models.CharField(max_length=100)
+    specialty = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class ResearchProject(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    start_date = models.DateField()
+    end_date = models.DateField()
+    project_lead = models.ForeignKey(Researcher, related_name='lead_projects')
+    researchers = models.ManyToManyField(Researcher, related_name='projects')
+
+    def __str__(self):
+        return self.title
+
+class Publication(models.Model):
+    title = models.CharField(max_length=200)
+    summary = models.TextField()
+    project = models.ForeignKey(ResearchProject, related_name='publications')
+    publication_date = models.DateField()
+
+    def __str__(self):
+        return self.title
